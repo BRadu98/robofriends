8590ceccbdf5aedc68c274154bd97555e1d91037
Baloi Radu <bradu2020@gmail.com>
2021-04-22 09:10:59 +0100

    cors

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,66 +10,71 @@ import Scroll from '../components/Scroll';
 //The virtual dom is a js obj, that collects this state
 //and react uses this state to render and pass em down as props to pure functions
 class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-        console.log('constructor');
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
     }
+    console.log('constructor');
+  }
 
-    //this is part of react, no need to use arrow func
-    componentDidMount() { //make requests here
+  //this is part of react, no need to use arrow func
+  componentDidMount() { //make requests here
 
-        // fetch('https://jsonplaceholder.typicode.com/users')
-        //     .then(response => {
-        //         return response.json();
-        // })
-        // .then(users => {
-        //     this.setState({ robots: users })
-        // })
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => {
+    //         return response.json();
+    // })
+    // .then(users => {
+    //     this.setState({ robots: users })
+    // })
 
-        //refactoring {} to oneliners //fetch is a method on the window obj
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+    const url = `${proxyUrl}https://jsonplaceholder.typicode.com/users`;
+    const request = new Request(url);
 
-        // this.setState({ robots: robots}) //using old robots.js file
-        console.log('componentDidMount');
-    }
+    //refactoring {} to oneliners //fetch is a method on the window obj
+    fetch(request)
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }))
+      .catch(e => console.log(e));
 
-    //in here "this" is not refering to app because 
-    //the event happened in searchbox.js's input so this is refr to input
-    // onSearchChange(event) { ......... }
-    //TO FIX use arrow func
-    onSearchChange = (event) => {
-        //*To update state, never this.state.searchfield, use setState
-        this.setState({ searchfield: event.target.value })
-    }
+    // this.setState({ robots: robots}) //using old robots.js file
+    console.log('componentDidMount');
+  }
 
-    render() {
-        console.log('render');
+  //in here "this" is not refering to app because 
+  //the event happened in searchbox.js's input so this is refr to input
+  // onSearchChange(event) { ......... }
+  //TO FIX use arrow func
+  onSearchChange = (event) => {
+    //*To update state, never this.state.searchfield, use setState
+    this.setState({ searchfield: event.target.value })
+  }
 
-        const { robots, searchfield } = this.state;
-        const filteredRobot = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
+  render() {
+    console.log('render');
 
-        return !robots.length ?
-            <h1>Loading</h1> :
-            (
-                <div className='tc'>
-                    <h1 className='f2'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <CardList robots={filteredRobot} />
-                    </Scroll>
+    const { robots, searchfield } = this.state;
+    const filteredRobot = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
 
-                </div>
-            )
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f2'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobot} />
+          </Scroll>
 
-    }
+        </div>
+      )
+
+  }
 
 }
 
